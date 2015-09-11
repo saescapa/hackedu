8060d1388cf02b636935bcbadcf78b6504ca52b7
Harrison <harrison@theshoebridges.com>
2015-09-12 07:53:08 +1000

    Fix undefined method

--- playbook/workshops/remote_control/src/main.js
+++ playbook/workshops/remote_control/src/main.js
@@ -1,4 +1,4 @@
-Phone.sendMessage("+1-484-238-5555", "WHATUP?!");
+Phone.sendSMS("+1-484-238-5555", "WHATUP?!");
 Phone.callAndSay("+1-484-238-5555", "Hello! My name is Jonathan! How are you?");
 Phone.callAndPlay("+1 (310) 303-5555", "http://a.tumblr.com/tumblr_lie8ewfdbO1qzbwpvo1.mp3");
 
