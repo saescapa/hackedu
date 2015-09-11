ce28d763e884d288f664361b8c4ab2126831b2e2
Harrison <harrison@theshoebridges.com>
2015-09-12 07:53:41 +1000

    Add basic rate limiting to colonel

--- playbook/workshops/remote_control/src/colonel.js
+++ playbook/workshops/remote_control/src/colonel.js
@@ -1,15 +1,61 @@
 // Twilio could just be assigned to the Phone variable, but that exposes a lot 
 // of functions. Best to be safe!
-var Phone = {};
+var Phone = {
+    canSend: true,
+    MAX_SENT: 20,
+    currentSent: 0,
+    duration: 500
+};
 
 Phone.sendSMS = function(to, content) {
+    if (!Phone.canSend) {
+        return;
+    }
+
     Twilio.sendMessage(to, content);
+
+    Phone._rate();
 }
 
 Phone.callAndSay = function(to, message) {
+    if (!Phone.canSend) {
+        return;
+    }
+
     Twilio.callAndSay(to, message);
+
+    Phone._rate();
 }
 
 Phone.callAndPlay = function(to, url) {
+    if (!Phone.canSend) {
+        return;
+    }
+
     Twilio.callAndPlay(to, url);
+
+    Phone._rate();
 }
+
+Phone._rate = function() {
+    Phone.currentSent += 1;
+
+    if (Phone.currentSent > Phone.MAX_SENT && Phone.canSend) {
+        console.info("You have been rate limited!")
+        return;
+    }
+
+    Phone.canSend = false;
+
+    setTimeout(function() {
+        Phone.canSend = true;
+        Phone.currentSent = 0;
+        Phone.duration *= 2;
+    }, Phone.duration);
+}
+
+setInterval(function() {
+    if (Phone.currentSent > 0) {
+        Phone.currentSent -= 1;
+    }
+}, 700);
--- playbook/workshops/remote_control/src/main.js
+++ playbook/workshops/remote_control/src/main.js
@@ -2,6 +2,10 @@ Phone.sendSMS("+1-484-238-5555", "WHATUP?!");
 Phone.callAndSay("+1-484-238-5555", "Hello! My name is Jonathan! How are you?");
 Phone.callAndPlay("+1 (310) 303-5555", "http://a.tumblr.com/tumblr_lie8ewfdbO1qzbwpvo1.mp3");
 
+document.getElementById("button").onclick = function() {
+    Phone.sendSMS("+1-555-555-5555", "Whats up dawg?");
+};
+
 Twilio.listenForMessages(function(message) {
   document.write(message.body);
 })
